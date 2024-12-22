18f4eac1790400b96eb5fd6a34e3f0e5739577b1
cioakeim <cioakeim@ece.auth.gr>
2024-12-22 21:28:09 +0200

    Test for new cmake config

--- misc/jobs/rbf_test_sweep.sh
+++ misc/jobs/rbf_test_sweep.sh
@@ -6,12 +6,15 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --time=4:00:00
-#SBATCH --array=0-9
+#SBATCH --array=0
 
 
 module load gcc/13.2.0-iqpfkya cmake/3.27.9-nmh6tto eigen/3.4.0-titj7ys 
 
-source /home/c/cioakeim/aocl/5.0.0/aocc/amd-libs.cfg
+MY_HOME="/home/c/cioakeim"
+
+source $MY_HOME/aocl/5.0.0/aocc/amd-libs.cfg
+source $MY_HOME/intel/oneapi/mkl/latest/env/vars.sh intel64
 
 sigma_list=("1e-3" "5e-3" "1e-2" "2.5e-2" "5e-2" "7.5e-2" "1e-1" "5e-1" "1" "5")
 
@@ -19,9 +22,10 @@ store_path="/home/c/cioakeim/nns/SVMs/RBF/test_$SLURM_ARRAY_TASK_ID"
 dataset_path="/home/c/cioakeim/nns/cifar-10-batches-bin"
 class_1_id="0"
 class_2_id="1"
-training_size="50000"
+training_size="20000"
 test_size="10000"
 C_list="1e-3,5e-3,1e-2,1e-1,1,5,10,50,100,1000"
+C_list="0.1"
 kernel_type="RBF"
 sigma="${sigma_list[$SLURM_ARRAY_TASK_ID]}"
 
@@ -31,7 +35,7 @@ project_dir="/home/c/cioakeim/nns/neural-networks-hw2"
 cd "$project_dir"
 mkdir -p build
 cd build
-cmake -DOSQP_MANUAL_LINK=ON -DBLAS_LIBRARIES="/home/c/cioakeim/aocl/5.0.0/aocc/lib/libblis.so" ..
+cmake -DMY_HOME_DIR="$MY_HOME"
 make
 
 
